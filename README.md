c27390bb408c2760dbd6248a112406ea23d550e7
SHYAM UNNITHAN <shyam.unnithan@nissanmotor.com>
2022-10-20 17:54:22 +0530

    Added functionality to \n 1) delete file from S3 post processing. \n 2) Upload information to DynamoDB

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # AWS PDF File Splitter.
 This lambda service helps split PDF files into pdfs page wise.
 
+The function is triggered when a file is uploaded in BucketInbound. The following actions are performed
+1) Check if the file is pdf
+2) If the uploaded file is PDF split the PDF pagewise into multiple PDF's and store it in BucketOutbound
+3) Delete the file from BucketInbound
+4) Use the first 4 characters of file as department name
+5) Upload the information regarding Department, Filename, Number of Pages and TimeStamp to DynamoDB
+
 ## AWS policy requirement
 Create a role with read write list access to S3 bucket
+
+Create a policy with 
+getItems
+putItems
+deleteItems
+listItems
+
+access to DynamoDB
+
+Add the above policies to the role used for running lambda services
 
 ## Using PikePDF on AWS
 Use the following instruction to create a zip file with the python dependencies</br>
--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+import time
 import urllib.parse
 from io import BytesIO
 
@@ -6,6 +7,8 @@ from pikepdf import Pdf
 
 bucketIn = 'bucketinbound'
 bucketOut = 'bucketoutbound'
+ts = time.time()
+
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
@@ -14,7 +17,10 @@ def lambda_handler(event, context):
     if key.endswith('.pdf'):
         bucketObject = s3.get_object(Bucket=bucketIn, Key=key)
         pdf = BytesIO(bucketObject['Body'].read())
-        splitPDF(pdf, key)
+        totalPages = splitPDF(pdf, key)
+        dept = key[0:4]
+        updateDynamoDB(dept, ts, key, totalPages)
+        s3.delete_object(Bucket=bucketIn, Key=key)
 
     # TODO implement
     return {
@@ -25,17 +31,23 @@ def lambda_handler(event, context):
 
 def splitPDF(fileBytes, key):
     pdf = Pdf.open(fileBytes)
+    totalPages = len(pdf.pages)
     for n, page in enumerate(pdf.pages):
         bio = BytesIO()
         output = Pdf.new()
         output.pages.append(page)
         output.save(bio)
         writePDF(bio, f'{key.split(".")[0]}_{n+1:03d}.pdf')
+    return totalPages
 
 
 def writePDF(bio, objectName):
-    print(objectName)
     s3 = boto3.resource('s3')
     boto_bucketOutbound = s3.Bucket(bucketOut)
     bio.seek(0)
     boto_bucketOutbound.upload_fileobj(bio, objectName)
+
+def updateDynamoDB(dept, ts, filename, totalPages):
+    dynamoDB = boto3.client('dynamodb')
+    dynamoDB.put_item(TableName = 'PDFSlicer', Item = {'Department' : {'S': dept}, 'Timestamp' : {'S':str(ts)}, 'FileName' : {'S': filename }, 'Pages' : {'N' : str(totalPages)}})
+    
